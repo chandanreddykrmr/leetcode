75917d100c9c04c187d4a093fc6f26c1e232df59
chandanreddykrmr <148303819+chandanreddykrmr@users.noreply.github.com>
2023-10-31 00:06:14 -0500

    Time: 1 ms (82.54%), Space: 39.8 MB (97.69%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,6 +6,7 @@ class Solution {
         for(int i = 1; i < strs.length; i++){
             length = Math.min(length, strs[i].length());
             
+            //will check for every char of the array elements
             for(int j = 0; j < length; j++){
                 char c = strs[0].charAt(j);
                 char d = strs[i].charAt(j);
