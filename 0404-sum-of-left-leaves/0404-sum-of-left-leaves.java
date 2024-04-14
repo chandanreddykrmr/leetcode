f7fe3df722170542367887fbd388315c46f15341
chandanreddykrmr <148303819+chandanreddykrmr@users.noreply.github.com>
2024-04-14 11:20:15 -0500

    Time: 1 ms (10.24%), Space: 41.5 MB (32.86%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -15,6 +15,9 @@
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
         int total = 0;
         Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.push(root);
